184956db9396261527ec57fad61563db2bb1d9ac
lxchinesszz <mac@MacBook-Air.local>
2017-07-03 22:59:28 +0800

    上传图片token

--- src/main/java/com/funny/service/impl/ImgEveryServiceImpl.java
+++ src/main/java/com/funny/service/impl/ImgEveryServiceImpl.java
@@ -3,7 +3,10 @@ package com.funny.service.impl;
 
 import com.funny.model.domain.ImageObj;
 import com.funny.service.ImgEveryService;
+import com.qiniu.util.Auth;
+import com.qiniu.util.StringMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,9 @@ import java.util.List;
  */
 @Service
 public class ImgEveryServiceImpl implements ImgEveryService {
+
+    @Autowired
+    Environment environment;
     @Autowired
     MongoTemplate mongoTemplate;
 
@@ -30,5 +36,18 @@ public class ImgEveryServiceImpl implements ImgEveryService {
     public List<ImageObj> getImageAsList() {
         List<ImageObj> imageObjList = mongoTemplate.findAll(ImageObj.class);
         return imageObjList;
+    }
+
+    @Override
+    public String getImageToken(String fileName) {
+        String ak = environment.getProperty("qiniu.ak");
+        String sk = environment.getProperty("qiniu.sk");
+        String bucket = environment.getProperty("qiniu.bucket");
+        String key = fileName;
+        long expireSeconds = 3600L;
+        Auth auth = Auth.create(ak, sk);
+        StringMap putPolicy = new StringMap();
+        putPolicy.put("returnBody", "{\"key\":\"$(key)\",\"hash\":\"$(etag)\",\"bucket\":\"$(bucket)\",\"fsize\":$(fsize)}");
+        return auth.uploadToken(bucket, key, expireSeconds, putPolicy);
     }
 }
